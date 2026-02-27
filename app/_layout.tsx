742a9b1d18b024a9d2534b309ef4307e12eb267d
Ben M <109357901+ben-me@users.noreply.github.com>
2026-02-27 17:11:17 +0100

    feat: Change styling of header, add new Icon and try some first styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+
+import { Colors } from "@/constants/theme";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const { backgroundColor, text } = Colors[colorScheme as "light" | "dark"];
+  const today = new Date().toLocaleDateString();
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor,
+        },
+        headerTitle: today,
+        headerTitleStyle: {
+          color: text,
+        },
+      }}
+    />
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
 import { open } from "@op-engineering/op-sqlite";
 import { drizzle } from "drizzle-orm/op-sqlite";
 import { useMigrations } from "drizzle-orm/op-sqlite/migrator";
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ScrollView, StyleSheet, Text, TextInput, useColorScheme, View } from "react-native";
 
+import IconAverage from "../components/icons/IconAverage";
+import { Colors } from "../constants/theme";
 import migrations from "../drizzle/migrations";
 
 const opsqliteDB = open({
@@ -12,7 +16,11 @@ const opsqliteDB = open({
 const db = drizzle(opsqliteDB);
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
   const { success, error } = useMigrations(db, migrations);
+  const [weight, onChangeWeight] = React.useState("");
+  const { text, backgroundColor } = Colors[colorScheme as "light" | "dark"];
 
   if (error) {
     console.error(error);
@@ -30,14 +38,48 @@ export default function Index() {
     );
   }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hallo Welt</Text>
+    <View style={[{ backgroundColor }, styles.container]}>
+      <StatusBar style="auto" />
+      <View style={[{ backgroundColor }, styles.inputContainer]}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ color: text }}>7 Tage </Text>
+          <IconAverage />
+        </View>
+        <TextInput
+          inputMode="decimal"
+          style={styles.input}
+          value={weight}
+          onChangeText={onChangeWeight}
+        />
+        <Text style={{ color: "red" }}>{backgroundColor}</Text>
+      </View>
+      <ScrollView style={[{ backgroundColor }]}></ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBlock: 20,
+    borderTopWidth: 1,
+    borderStyle: "solid",
+    borderColor: "gray",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    borderStyle: "solid",
+    borderBottomWidth: 2,
+    borderBottomColor: "white",
+  },
+  input: {
+    borderStyle: "solid",
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    width: 70,
+  },
+  display: {},
+  weightList: {},
+});
