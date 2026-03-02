7c739951c84e046b603ed6605343ae56f81fab27
Ben M <109357901+ben-me@users.noreply.github.com>
2026-03-02 20:32:33 +0100

    feat: Add type for table entry and make unit not null and have default kg

--- db/schema.ts
+++ db/schema.ts
@@ -3,5 +3,7 @@ import { real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const weightTable = sqliteTable("weight_table", {
   date: text("date").primaryKey(),
   weight: real("weight"),
-  unit: text("unit"),
+  unit: text("unit").notNull().default("KG"),
 });
+
+export type DataEntry = typeof weightTable.$inferSelect;
