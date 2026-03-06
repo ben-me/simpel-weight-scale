39d112610bc0410c06a5aa16939b412af26dafc7
Ben M <109357901+ben-me@users.noreply.github.com>
2026-03-06 16:39:05 +0100

    feat: Add rightIcon prop for Input

--- components/ThemedInput.tsx
+++ components/ThemedInput.tsx
@@ -1,17 +1,30 @@
-import { StyleSheet, TextInput, TextInputProps, useColorScheme } from "react-native";
+import { ReactNode } from "react";
+import { StyleSheet, TextInput, TextInputProps, useColorScheme, View } from "react-native";
 
 import { Colors } from "@/constants/theme";
 
-export default function ThemedInput({ style, ...rest }: TextInputProps) {
+type Props = {
+  rightIcon?: ReactNode;
+} & TextInputProps;
+
+export default function ThemedInput({ rightIcon, style, ...rest }: Props) {
   const colorScheme = useColorScheme();
   const { text } = Colors[colorScheme ?? "light"];
 
   return (
-    <TextInput {...rest} style={[{ color: text, borderColor: text }, styles.default, style]} />
+    <View style={styles.wrapper}>
+      <TextInput {...rest} style={[{ color: text, borderColor: text }, styles.default, style]} />
+      {rightIcon && <View>{rightIcon}</View>}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "row",
+    gap: 4,
+    alignItems: "center",
+  },
   default: {
     borderBottomWidth: 1,
     width: 160,
