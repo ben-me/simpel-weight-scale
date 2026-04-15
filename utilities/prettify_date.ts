ce511b06ff79f967a06b95b97d64a71884dae1ee
Ben M <109357901+ben-me@users.noreply.github.com>
2026-04-15 13:27:30 +0200

    feat: add locale to weight list item display

--- utilities/prettify_date.ts
+++ utilities/prettify_date.ts
@@ -3,13 +3,19 @@
  * @returns A date string looking like "Mo., 31.12.2025"
  */
 
+import { useTranslation } from "react-i18next";
+
 export function prettifyDate(date: string) {
+  const { i18n } = useTranslation();
   const [year, month, day] = date.split("-");
-  const formatted_display_date = new Date(+year, +month - 1, +day).toLocaleDateString(undefined, {
-    weekday: "short",
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+  const formatted_display_date = new Date(+year, +month - 1, +day).toLocaleDateString(
+    i18n.language,
+    {
+      weekday: "short",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    },
+  );
   return formatted_display_date;
 }
