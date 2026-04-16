54380bde211d4e70ce6aa5bf5c30cc243d31237c
Ben M <109357901+ben-me@users.noreply.github.com>
2026-04-16 17:16:15 +0200

    refactor: make unit be not empty but without default value

--- db/schema.ts
+++ db/schema.ts
@@ -3,7 +3,7 @@ import { real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const weightTable = sqliteTable("weight", {
   date: text().primaryKey(),
   weight: real(),
-  unit: text().default("KG"),
+  unit: text().notNull(),
 });
 
 export const settings = sqliteTable("settings", {
